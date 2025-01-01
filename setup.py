c1124244a537c8aab4d4b5a0e97067fd60ed58b1
Gaurav Jaiswal <jaiswalgaurav863@gmail.com>
2025-01-01 18:43:02 +0530

    folder stucture added

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name="KidneyDiseaseClassification",
+    version="0.0.1",
+    author="Gaurav Jaiswal",
+    author_email="jaiswalgaurav863@gmail.com",
+    description="A small package for Kidney Disease Classification.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Gaurav-Jaiswal-1/Kidney-Disease-Classification",
+    package_dir={"": "src"},
+)
